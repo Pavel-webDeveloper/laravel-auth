991cd245eb1b942f22101c834c8c88da8dad5417
Pavel-webDeveloper <paveldevjob@gmail.com>
2025-08-29 13:15:01 +0200

    Creata la model migration Movie + seeder con 10 movie per popolare inizialmente il database

---
+++ config/movies.php
@@ -0,0 +1,86 @@
+<?php
+
+  return [
+    [
+        'titolo' => 'Inception',
+        'regista' => 'Christopher Nolan',
+        'durata' => '02:28:00',
+        'anno' => 2010,
+        'nazione' => 'USA',
+        'pubblicato' => true,
+    ],
+    [
+        'titolo' => 'La grande bellezza',
+        'regista' => 'Paolo Sorrentino',
+        'durata' => '02:21:00',
+        'anno' => 2013,
+        'nazione' => 'Italia',
+        'pubblicato' => true,
+    ],
+    [
+        'titolo' => 'Parasite',
+        'regista' => 'Bong Joon-ho',
+        'durata' => '02:12:00',
+        'anno' => 2019,
+        'nazione' => 'Corea del Sud',
+        'pubblicato' => true,
+    ],
+    [
+        'titolo' => 'Amélie',
+        'regista' => 'Jean-Pierre Jeunet',
+        'durata' => '02:02:00',
+        'anno' => 2001,
+        'nazione' => 'Francia',
+        'pubblicato' => false,
+    ],
+    [
+        'titolo' => 'Il favoloso mondo di Oz',
+        'regista' => 'Victor Fleming',
+        'durata' => '01:41:00',
+        'anno' => 1939,
+        'nazione' => 'USA',
+        'pubblicato' => false,
+    ],
+    [
+        'titolo' => 'Spirited Away',
+        'regista' => 'Hayao Miyazaki',
+        'durata' => '02:05:00',
+        'anno' => 2001,
+        'nazione' => 'Giappone',
+        'pubblicato' => true,
+    ],
+    [
+        'titolo' => 'The Godfather',
+        'regista' => 'Francis Ford Coppola',
+        'durata' => '02:55:00',
+        'anno' => 1972,
+        'nazione' => 'USA',
+        'pubblicato' => true,
+    ],
+    [
+        'titolo' => 'Roma',
+        'regista' => 'Alfonso Cuarón',
+        'durata' => '02:15:00',
+        'anno' => 2018,
+        'nazione' => 'Messico',
+        'pubblicato' => false,
+    ],
+    [
+        'titolo' => 'Il postino',
+        'regista' => 'Michael Radford',
+        'durata' => '01:48:00',
+        'anno' => 1994,
+        'nazione' => 'Italia',
+        'pubblicato' => true,
+    ],
+    [
+        'titolo' => "Pan's Labyrinth",
+        'regista' => 'Guillermo del Toro',
+        'durata' => '01:58:00',
+        'anno' => 2006,
+        'nazione' => 'Spagna',
+        'pubblicato' => false,
+    ]
+  ];
+
+?>
